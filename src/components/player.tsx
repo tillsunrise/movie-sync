50af2e45c3db4a8f76096551b84a843b61b80d13
tillsunrise <71312152+tillsunrise@users.noreply.github.com>
2024-05-29 07:49:20 +0000

    fix

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -74,6 +74,9 @@ export const Player = ({ roomName }: { roomName: string }) => {
             }
             const maxtime = Math.max(...msg.userStatus.map(user => user?.time ?? 0));
             console.log(maxtime)
+
+            while(!player.current){new Promise(resolve => setTimeout(resolve, 1000));}
+            
             if (player.current && Math.abs(player.current.currentTime - maxtime) > 1) {
                 player.current.currentTime = maxtime
                 socket.emit("setTime", JSON.stringify({
